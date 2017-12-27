a0a06a3c4dbaf805d9ea4142c676fcca29c7254f
songchaow <songchaow@outlook.com>
2017-12-27 20:48:58 +0800

    format codes

--- Lab3/source/tree.cpp
+++ Lab3/source/tree.cpp
@@ -11,12 +11,12 @@ class BinSearchTree
         TreeNode *p;
         bool isred;
     } Node;
-    
+
     Node *search(ElementT key)
     {
         return _search(key, head);
     }
-    Node* insert(ElementT key)
+    Node *insert(ElementT key)
     {
         auto node_ptr = new TreeNode;
         node_ptr->lchild = nullptr;
@@ -121,9 +121,9 @@ class BinSearchTree
             // there are three parts: p, p_succ, p_succ->right
             // 8 operations are needed
             // determine p's father
-            
-            if(p==BinSearchTree<ElementT>::head)
-                BinSearchTree<ElementT>::head = p;
+
+            if (p == BinSearchTree<ElementT>::head)
+                BinSearchTree<ElementT>::head = p_succ;
             else
             {
                 if (p->p->lchild == p)
@@ -134,7 +134,7 @@ class BinSearchTree
 
             p_succ->lchild = p->lchild;
             p->lchild->p = p_succ;
-            if(p_succ != p->rchild)
+            if (p_succ != p->rchild)
             {
                 p_succ->p->lchild = p_succ->rchild;
                 if (p_succ->rchild)
@@ -143,7 +143,7 @@ class BinSearchTree
                 p->rchild->p = p_succ;
             }
             p_succ->p = p->p;
-            
+
             delete p;
         }
     }
@@ -174,64 +174,63 @@ class BinSearchTree
 };
 
 template <typename ElementT>
-class RedBlackTree:public BinSearchTree<ElementT>
+class RedBlackTree : public BinSearchTree<ElementT>
 {
-private:
-    void leftRotate(typename BinSearchTree<ElementT>::Node* x)
+  private:
+    void leftRotate(typename BinSearchTree<ElementT>::Node *x)
     {
-        if(x->rchild == nullptr)
+        if (x->rchild == nullptr)
             return;
-        
+
         auto new_root = x->rchild;
-        if(BinSearchTree<ElementT>::head == x)
+        if (BinSearchTree<ElementT>::head == x)
             BinSearchTree<ElementT>::head = new_root; // no need to change links to upper nodes
         else
         {
-            if(x->p->lchild==x)
+            if (x->p->lchild == x)
                 x->p->lchild = new_root;
             else
                 x->p->rchild = new_root;
         }
         new_root->p = x->p;
-        
+
         x->rchild = new_root->lchild;
-        if(new_root->lchild)
+        if (new_root->lchild)
             new_root->lchild->p = x->rchild;
 
         x->p = new_root;
         new_root->lchild = x;
-        
     }
-    void rightRotate(typename BinSearchTree<ElementT>::Node* x)
+    void rightRotate(typename BinSearchTree<ElementT>::Node *x)
     {
-        if(x->lchild == nullptr)
+        if (x->lchild == nullptr)
             return;
         auto new_root = x->lchild;
-        if(BinSearchTree<ElementT>::head == x)
+        if (BinSearchTree<ElementT>::head == x)
             BinSearchTree<ElementT>::head = new_root; // no need to change links to upper nodes
         else
         {
-            if(x->p->lchild==x)
+            if (x->p->lchild == x)
                 x->p->lchild = new_root;
             else
                 x->p->rchild = new_root;
             new_root->p = x->p;
         }
         x->lchild = new_root->rchild;
-        if(new_root->rchild)
+        if (new_root->rchild)
             new_root->rchild->p = x->lchild;
 
         x->p = new_root;
         new_root->rchild = x;
     }
-    void delete_fixup(typename BinSearchTree<ElementT>::Node* x, typename BinSearchTree<ElementT>::Node* x_father=nullptr)
+    void delete_fixup(typename BinSearchTree<ElementT>::Node *x, typename BinSearchTree<ElementT>::Node *x_father = nullptr)
     {
-        while(x!=BinSearchTree<ElementT>::head && ((x==nullptr) || (x->isred==false)))
+        while (x != BinSearchTree<ElementT>::head && ((x == nullptr) || (x->isred == false)))
         {
-            if(x==x_father->lchild)
+            if (x == x_father->lchild)
             {
                 auto w = x_father->rchild;
-                if(w->isred)
+                if (w->isred)
                 {
                     // CASE I
                     w->isred = false;
@@ -239,15 +238,15 @@ private:
                     leftRotate(x_father);
                     w = x_father->rchild; // we can prove that in this cse, w is always not null, but w's children may not
                 }
-                if((w->lchild==nullptr || w->lchild->isred==false) && (w->rchild==nullptr || w->rchild->isred==false))
-                {   // CASE II
+                if ((w->lchild == nullptr || w->lchild->isred == false) && (w->rchild == nullptr || w->rchild->isred == false))
+                { // CASE II
                     w->isred = true;
                     x = x_father;
                     x_father = x_father->p;
                 }
-                else if(w->rchild==nullptr || w->rchild->isred == false)
-                {   // CASE III
-                    if(w->lchild)
+                else if (w->rchild == nullptr || w->rchild->isred == false)
+                { // CASE III
+                    if (w->lchild)
                         w->lchild->isred = false;
                     w->isred = true;
                     rightRotate(w);
@@ -255,7 +254,7 @@ private:
                 }
                 w->isred = x_father->isred;
                 x_father->isred = false;
-                if(w->rchild)
+                if (w->rchild)
                     w->rchild->isred = false;
                 leftRotate(x_father);
                 x = BinSearchTree<ElementT>::head;
@@ -263,7 +262,7 @@ private:
             else
             {
                 auto w = x_father->lchild;
-                if(w->isred)
+                if (w->isred)
                 {
                     // CASE I
                     w->isred = false;
@@ -271,15 +270,15 @@ private:
                     rightRotate(x_father);
                     w = x_father->lchild; // we can prove that in this cse, w is always not null, but w's children may not
                 }
-                if((w->rchild==nullptr || w->rchild->isred==false) && (w->lchild==nullptr || w->lchild->isred==false))
-                {   // CASE II
+                if ((w->rchild == nullptr || w->rchild->isred == false) && (w->lchild == nullptr || w->lchild->isred == false))
+                { // CASE II
                     w->isred = true;
                     x = x_father;
                     x_father = x_father->p;
                 }
-                else if(w->lchild==nullptr || w->lchild->isred == false)
-                {   // CASE III
-                    if(w->rchild)
+                else if (w->lchild == nullptr || w->lchild->isred == false)
+                { // CASE III
+                    if (w->rchild)
                         w->rchild->isred = false;
                     w->isred = true;
                     leftRotate(w);
@@ -287,28 +286,31 @@ private:
                 }
                 w->isred = x_father->isred;
                 x_father->isred = false;
-                if(w->lchild)
+                if (w->lchild)
                     w->lchild->isred = false;
                 rightRotate(x_father);
                 x = BinSearchTree<ElementT>::head;
             }
         }
     }
-    
-public:
+
+  public:
     void insert(ElementT key)
     {
         auto node = BinSearchTree<ElementT>::insert(key);
         node->isred = true;
-        if(node->p == nullptr) // it's root itself.
-            {node->isred = false; return;}
-        while(node->p && node->p->isred)
+        if (node->p == nullptr) // it's root itself.
+        {
+            node->isred = false;
+            return;
+        }
+        while (node->p && node->p->isred)
         // we can tell that node->p->p exists!
         {
-            if(node->p == node->p->p->lchild)
+            if (node->p == node->p->p->lchild)
             {
                 auto y = node->p->p->rchild;
-                if(y && y->isred)
+                if (y && y->isred)
                 {
                     // CASE I
                     node->p->isred = false;
@@ -318,9 +320,9 @@ public:
                 }
                 else
                 {
-                    if(node == node->p->rchild)
+                    if (node == node->p->rchild)
                     {
-                    // CASE II
+                        // CASE II
                         node = node->p;
                         leftRotate(node);
                     }
@@ -333,7 +335,7 @@ public:
             else
             {
                 auto y = node->p->p->lchild;
-                if(y && y->isred)
+                if (y && y->isred)
                 {
                     // CASE I
                     node->p->isred = false;
@@ -343,9 +345,9 @@ public:
                 }
                 else
                 {
-                    if(node == node->p->lchild)
+                    if (node == node->p->lchild)
                     {
-                    // CASE II
+                        // CASE II
                         node = node->p;
                         rightRotate(node);
                     }
@@ -353,18 +355,17 @@ public:
                     node->p->isred = false;
                     node->p->p->isred = true;
                     leftRotate(node->p->p);
-
                 }
             }
         }
         BinSearchTree<ElementT>::head->isred = false;
     }
-    void delete_(typename BinSearchTree<ElementT>::Node* p)
+    void delete_(typename BinSearchTree<ElementT>::Node *p)
     {
         //  set default color for CASE I and II
         auto original_color = p->isred;
         //  x: the analyse starting point for red-black fix
-        typename BinSearchTree<ElementT>::Node* x;
+        typename BinSearchTree<ElementT>::Node *x;
         //  CASE I: there are no children in this node, which must be fixed
         if (p->lchild == nullptr && p->rchild == nullptr)
         {
@@ -372,7 +373,7 @@ public:
                 p->p->rchild = nullptr;
             else if (p->p->lchild == p)
                 p->p->lchild = nullptr;
-            delete_fixup(nullptr,p->p);
+            delete_fixup(nullptr, p->p);
             delete p;
         }
         //  CASE II: one of the two children is null, which not neccessarily needed to be fixed
@@ -388,11 +389,12 @@ public:
                 p->p->lchild = p->rchild;
                 p->rchild->p = p->p;
             }
-            if(!(p->isred))
+            if (!(p->isred))
             {
-                if(p->rchild)
+                if (p->rchild)
                     delete_fixup(p->rchild);
-                else delete_fixup(p->rchild, p->p);
+                else
+                    delete_fixup(p->rchild, p->p);
             }
             delete p;
         }
@@ -408,11 +410,12 @@ public:
                 p->p->lchild = p->lchild;
                 p->lchild->p = p->p;
             }
-            if(!(p->isred))
+            if (!(p->isred))
             {
-                if(p->lchild)
+                if (p->lchild)
                     delete_fixup(p->lchild);
-                else delete_fixup(p->lchild, p->p);
+                else
+                    delete_fixup(p->lchild, p->p);
             }
             delete p;
         }
@@ -431,8 +434,8 @@ public:
             // there are three parts: p, p_succ, p_succ->right
             // 8 operations are needed
             // determine p's father
-            if(p==BinSearchTree<ElementT>::head)
-                BinSearchTree<ElementT>::head = p;
+            if (p == BinSearchTree<ElementT>::head)
+                BinSearchTree<ElementT>::head = p_succ;
             else
             {
                 if (p->p->lchild == p)
@@ -443,7 +446,7 @@ public:
 
             p_succ->lchild = p->lchild;
             p->lchild->p = p_succ;
-            if(p_succ != p->rchild)
+            if (p_succ != p->rchild)
             {
                 p_succ->p->lchild = p_succ->rchild;
                 if (p_succ->rchild)
@@ -453,15 +456,14 @@ public:
             }
             p_succ->p = p->p;
             // check if neccessary to fix up
-            if(original_color == false)
+            if (original_color == false)
             {
-                if(z == nullptr)
-                    delete_fixup(z,p_father);
+                if (z == nullptr)
+                    delete_fixup(z, p_father);
                 else
                     delete_fixup(z);
             }
             delete p;
         }
     }
-
 };
